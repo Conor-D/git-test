cc88f7cb3f6216e4f65699286e670840f776188f
Conor-D <conor.9277@gmail.com>
2018-07-28 10:59:03 +0800

    add user output.

---
+++ output.ts
@@ -0,0 +1,5 @@
+import { User } from "./test";
+
+function outPut(user: User) {
+    console.log(user.name, user.password);
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,4 @@
-'' 
+export class User {
+    name: string;
+    password: string;
+}
